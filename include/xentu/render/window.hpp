04ac9b835238744ffb01cff4f450fa2e2f1c626d
Daniel Oh <odocodani@gmail.com>
2022-01-23 00:01:55 +0900

    Move window class into xentu namespace

--- include/xentu/render/window.hpp
+++ include/xentu/render/window.hpp
@@ -1,19 +1,22 @@
 #pragma once
 
-class Window
+namespace xentu
 {
-private:
-   int width, height;
-   const char *caption;
-   bool fullscreen;
+   class Window
+   {
+   private:
+      int width, height;
+      const char *caption;
+      bool fullscreen;
 
-public:
-   Window(int width, int height, const char *caption, bool fullscreen);
+   public:
+      Window(int width, int height, const char *caption, bool fullscreen);
 
-   inline int GetWidth() { return width; }
-   inline int GetHeight() { return height; }
-   inline const char *GetCaption() { return caption; }
-   inline bool IsFullscreen() { return fullscreen; }
+      inline int GetWidth() { return width; }
+      inline int GetHeight() { return height; }
+      inline const char *GetCaption() { return caption; }
+      inline bool IsFullscreen() { return fullscreen; }
 
-   void Draw();
-}
+      void Draw();
+   };
+} // namespace xentu
