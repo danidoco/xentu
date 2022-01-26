4b3e83479ba8deb642fd52eadeb494b37518e863
Daniel Oh <odocodani@gmail.com>
2022-01-26 09:49:36 +0900

    Improve conditional build

--- sandbox/src/main.cpp
+++ sandbox/src/main.cpp
@@ -9,8 +9,11 @@ public:
    Sandbox()
       : Application("Sandbox")
    {
+      XN_TRACE("Hello world");
       XN_INFO("{} running!", GetApplicationName());
       XN_WARN("Warning from {}", GetApplicationName());
+      XN_ERROR("HELLWORLD");
+      XN_CRITICAL("CRITICAL!!!");
 
       window = xentu::CreateWindow(this, 800, 800, "Sandbox", false);
    }  
